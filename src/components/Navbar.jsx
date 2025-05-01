6704c8557948103a6923c748aebd2423a8bcdfff
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2025-05-01 23:33:23 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from '../assets/logo-white.png'
 import resume from '../assets/resume.pdf'
 import { FaLinkedin } from 'react-icons/fa'
@@ -8,11 +7,14 @@ import { FaInstagram } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div className='mb-20 flex items-center justify-between py-6'>
-      <div className='flex flex-shrink-0 items-center'>
-        <img src={logo} className='mx-2 w-32' alt="logo" />
+    <div className='mb-12 px-4 py-6 md:flex md:items-center md:justify-between'>
+      {/* Logo */}
+      <div className='flex justify-center md:justify-start'>
+        <img src={logo} className='w-32' alt="logo" />
       </div>
-      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+
+      {/* Social Links + Resume Button */}
+      <div className='mt-4 flex flex-col items-center gap-4 text-2xl md:mt-0 md:flex-row md:gap-6'>
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/Nithinlinga"><FaGithub /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/nithin-linga-09235a226/"><FaLinkedin /></a>
         <a target="_blank" rel="noopener noreferrer" href="https://x.com/nithinkohli5"><FaSquareXTwitter /></a>
@@ -21,7 +23,7 @@ const Navbar = () => {
         <a
           href={resume}
           download="LingaNithin_2025_SDE.pdf"
-          className="ml-4 rounded bg-white px-4 py-2 text-sm font-semibold text-black transition hover:bg-gray-200"
+          className="rounded bg-white px-4 py-2 text-sm font-semibold text-black transition hover:bg-gray-200"
         >
           Download Resume
         </a>
