f2447344e1c6ca5f1be4ba5e1afd3d500b3ae19c
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2025-05-11 13:40:08 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,7 +20,7 @@ export const PROJECTS = [
   {
     title: "Url Shortener",
     image: project5,
-    link:"https://url-shorten-sb.netlify.app/",
+    link:"https://url-shorten-rc.netlify.app/",
     description:
       "A URL shortener with analytics allows users to create compact links while tracking their performance. It provides insights like total clicks, traffic sources, and user engagement over time.",
     technologies: ["React.js", "Tailwind CSS", "Chart.js","Java","Spring Boot","PostgreSQL"]
