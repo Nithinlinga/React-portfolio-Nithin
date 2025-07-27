4a0032e67d851a9b65cfc482dca607639e29f342
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2025-07-27 12:14:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './components/About'
 import Technologies from './components/Technologies'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import Contact from './components/Experience'
+import Experience from './components/Experience'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 
 
