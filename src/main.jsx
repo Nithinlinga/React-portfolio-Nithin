a86b74d0357735710ba8a86f8812b68606442d48
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2025-11-22 16:39:24 +0000

    added cursor

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
-
+import UseAnimationFrame from './UseAnimationFrame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <UseAnimationFrame/>
     </ThemeProvider>
   </StrictMode>,
 )
