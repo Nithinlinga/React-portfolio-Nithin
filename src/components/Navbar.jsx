1bbb210dd2dd1a45ec20ce3fd130062b774efc96
Nithinlinga <nithinlinga2@gmail.com>
2025-12-30 11:14:11 +0530

    linkedIn Url Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
 
   const navLinks = [
     { name: 'GitHub', href: 'https://github.com/Nithinlinga', icon: FaGithub },
-    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/nithin-linga-09235a226/', icon: FaLinkedin },
-    { name: 'Twitter', href: 'https://x.com/nithinkohli5', icon: FaSquareXTwitter },
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/in/nithinlinga/', icon: FaLinkedin },
+    { name: 'X', href: 'https://x.com/nithinkohli5', icon: FaSquareXTwitter },
   ];
   const resume="https://drive.google.com/file/d/1r7WiPcCtK03Bkoed8CGmdYvoOvirte36/view?usp=drive_link";
 
